458d105b0cde780156384f2f51234b11aebe7d89
mamutdin <mamyt1993@mail.ru>
2022-04-09 15:41:42 +0300

    work_code v.2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
-
+import json
+from tqdm import tqdm
 import requests
 from pprint import pprint
+
+
 
 screen_name = 'begemot_korovin'
 def get_user_id(name=str):
@@ -15,18 +18,29 @@ def get_photos(id):
     token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
     album = 'profile'
     url = 'https://api.vk.com/method/photos.get'
-    params = {'owner_id': id, 'album_id': album, 'access_token': token, 'v': '5.131'}
+    params = {'owner_id': id, 'album_id': album, 'access_token': token, 'extended': 1, 'v': '5.131'}
     res = requests.get(url, params=params)
     photos = res.json()['response']['items']
+
+    # pprint(photos)
+    big_photos =[]
     photo_big = []
     for photo in photos:
         # print(photo['sizes'])
-        for p in photo['sizes']:
-            if p['type'] == 'x':
-                photo_big.append(p['url'])
+        sorted_photos = sorted(photo['sizes'], key=lambda x: x['height'])
+        sorted_photos.reverse()
+        sorted_photos[0]['likes'] = photo['likes']['count']
+        big_photos.append(sorted_photos[0])
+
+        photo_big.append(sorted_photos[0]['url'])
+
+    with open('data.txt', 'w') as outfile:
+        json.dump(big_photos, outfile, indent=4)
+
     return photo_big
 
 photos = get_photos(get_user_id(screen_name))
+pprint(photos)
 
 class YaUploader:
     def __init__(self, token: str):
@@ -42,12 +56,11 @@ class YaUploader:
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
                    'Authorization': f'OAuth {self.token}'}
-        for p in photos:
+
+        for p in tqdm(photos):
             params2 = {'path': f'{folder}/{file}', 'url': p}
             response = requests.post(upload_url, headers=headers, params=params2)
-            pprint(response)
-            time.sleep(1)
-
+            time.sleep(0.5)
 
 token = 'AQAAAAAnTSzNAADLW-jve_JHdkqztQk1d2ApBtE'
 folder = 'Backup'
