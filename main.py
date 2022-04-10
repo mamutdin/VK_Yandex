5751ac043c1b452d45440bf5eeb2f07607cbfb96
mamutdin <mamyt1993@mail.ru>
2022-04-10 13:21:04 +0300

    work_code final

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import time
 import json
 from tqdm import tqdm
 import requests
-from pprint import pprint
 
-
-
-screen_name = 'begemot_korovin'
 def get_user_id(name=str):
     token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
     url = 'https://api.vk.com/method/users.get'
@@ -24,19 +20,26 @@ def get_photos(id):
 
     json_file =[]
     photo_big = []
+    ph = []
     for photo in photos:
         sorted_photos = sorted(photo['sizes'], key=lambda x: x['height'])
         sorted_photos.reverse()
-        # photo_big.append(sorted_photos[0]['url'])
-        photo_big.append({'file_name': f"{photo['likes']['count']}.jpeg", 'size': sorted_photos[0]['type'], 'url': sorted_photos[0]['url']})
-        json_file.append({'file_name': f"{photo['likes']['count']}.jpeg", 'size': sorted_photos[0]['type']})
+        p_name = photo['likes']['count']
+        p_size = sorted_photos[0]['type']
+        p_url = sorted_photos[0]['url']
+
+        if photo['likes']['count'] in ph:
+            json_file.append({'file_name': f"{p_name}_{photo['date']}.jpeg", 'size': p_size})
+            photo_big.append({'file_name': f"{p_name}_{photo['date']}.jpeg", 'size': p_size, 'url': p_url})
+        else:
+            json_file.append({'file_name': f"{p_name}.jpeg", 'size': p_size})
+            photo_big.append({'file_name': f"{p_name}.jpeg", 'size': p_size, 'url': p_url})
+        ph.append(p_name)
 
     with open('data.txt', 'w') as outfile:
         json.dump(json_file, outfile, indent=4)
 
     return photo_big
-
-uploaded_photos = get_photos(get_user_id(screen_name))
 
 class YaUploader:
     def __init__(self, token: str):
@@ -47,16 +50,6 @@ class YaUploader:
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {self.token}'}
         params = {'path': folder}
         r = requests.put(url, headers=headers, params=params).json()
-
-    # def upload(self, folder: str, file: str, photos:list):
-    #     upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-    #     headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
-    #                'Authorization': f'OAuth {self.token}'}
-    #
-    #     for p in tqdm(photos):
-    #         params2 = {'path': f'{folder}/{file}', 'url': p}
-    #         response = requests.post(upload_url, headers=headers, params=params2)
-    #         time.sleep(0.5)
 
     def upload(self, folder: str, photos:list):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
@@ -69,8 +62,10 @@ class YaUploader:
             time.sleep(0.5)
 
 token = 'AQAAAAAnTSzNAADLW-jve_JHdkqztQk1d2ApBtE'
+
+screen_name = 'begemot_korovin'
 folder = 'Backup'
-pprint(uploaded_photos)
+uploaded_photos = get_photos(get_user_id(screen_name))
 
 uploader = YaUploader(token)
 result = uploader.create_folder(folder)
